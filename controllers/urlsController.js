b7cc26dc5693a4353659d4520f694c03480084ef
Gleison Moura <gle.bar.mou@gmail.com>
2022-06-17 00:58:50 -0300

    put endpoit to open short url and redirect the client

--- controllers/urlsController.js
+++ controllers/urlsController.js
@@ -39,9 +39,43 @@ export async function getURLById(req,res) {
 }
 
 export async function deleteURL(req,res) {
-    
+    const {id} = req.params;
+    const {user} = res.locals;
+
+    try{
+        const thisUrl = db.query(`SELECT * FORM urls WHERE id=$1`, [id]);
+        if(!thisUrl.rowCount === 0) {
+            res.sendStatus(404)
+        }
+
+        const {url} = thisUrl.rows;
+        if(url.userId !== user.id) {
+            return res.sendStatus(401);
+        }
+
+        await db.query(`DELETE FROM urls WHERE id=$1`, [id]);
+        res.sendStatus(401);
+    }catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
 }
 
 export async function openShortURL(req,res) {
-    
+    const {shortUrl} = req.params;
+    try{
+        const thisUrl = db.query(`SELECT * FROM urls WHERE 'shortUrl'=$1`, [shortUrl]);
+        if(!thisUrl) {
+            return res.sendStatus(404);
+        }
+        const {url} = thisUrl.rows;
+
+        await db.query(`UPDATE urls SET 'visitCount'='visitCount'+1 WHERE id=$1`, [url.id]);
+
+        res.redirect(url.url);
+
+    }catch(error){
+        console.log(error);
+        res.sendStatus(500);
+    }   
 }
